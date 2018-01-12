7db4d6fe5474ebe2da8c17f36a4ef6581e521083
José Romildo Malaquias <malaquias@gmail.com>
2018-01-12 07:58:40 -0200

    Disable shackle rule for  grep-mode

--- init.el
+++ init.el
@@ -2394,7 +2394,7 @@ XLFD defaults to the selected frame's font, or the default face's font."
 
 (use-package shackle ; Set rules for popup buffers
   :ensure
-  :custom (shackle-rules '((grep-mode :select t)))
+  ;;:custom (shackle-rules '((grep-mode :select t)))
   :config (shackle-mode 1))
 
 ;;;----------------------------------------------------------------------------
