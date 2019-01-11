f157d4eed45bf998b6afba988097605803d3de55
José Romildo Malaquias <malaquias@gmail.com>
2019-01-11 10:23:17 -0200

    Use PREVIEWER variable

--- bin/print-preview.sh
+++ bin/print-preview.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+PREVIEWER=okular
+
 PREVIEW_FILE=$(mktemp --suffix=.ps)
-cat >"$PREVIEW_FILE" && evince "$PREVIEW_FILE" || \
+
+cat >"$PREVIEW_FILE" && $PREVIEWER "$PREVIEW_FILE" || \
     notify-send 'Print preview failed' --urgency=critical \
     --expire-time=2000
+
 rm "$PREVIEW_FILE"
